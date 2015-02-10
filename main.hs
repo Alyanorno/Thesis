8a82b3d9e7f8ef5eead7c6c18d626a3355ecf91e
Rickard Fridvall <patim@live.com>
2015-02-10 15:55:12 +0100

    A new birth

--- main.hs
+++ main.hs
@@ -2,12 +2,12 @@
 
 
 
-
-
 type ID = Int
-data Person = { id :: ID } deriving (Show)
-data Relation = { id1 :: ID, id2 :: ID } deriving (Show)
+data Person = Person { id :: ID } deriving (Show)
 type People = [Person]
+
+data Connection = Parrent | Sibling | Lover deriving (Show, Eq)
+data Relation = Relation { connection :: Connection, persons :: (ID, ID) } deriving (Show)
 type Relations = [Relation]
 
 
@@ -28,17 +28,46 @@ type Relations = [Relation]
 -- Check for pair and birth probability and add new people
 
 
-birth :: [(People,Relations)] -> [(People,Relations)]
-birth a = a
+birth :: (People,Relations) -> (People,Relations)
+birth a = (fst r, snd latest ++ snd r)
+	where 
+		r = foldr1 (\a b -> (fst a ++ fst b, snd a ++ snd b)) $ map f lovers
+			where
+				f l = (p, c)
+				p = [Person i | i <- [0..3]] -- TODO: Must change to proper id
+				c = parrents ++ siblings
+				parrents = [[Relation Parrent ((id c), (id p)) | c <- p] | p <- [fst l, snd l]]
+				siblings = foo p
+					where foo s
+						| length s > 1 = [Relation Sibling (id (head s)) c | c <- (tail s)] ++ foo (tail s)
+						| otherwise = []
+		latest = (last a)
+		lovers = filter ((==Lover).connection) (snd latest)
+
 
 death :: [(People,Relations)] -> [(People,Relations)]
 death a = a
+
 
 change :: [(People,Relations)] -> [(People,Relations)]
 change a = a
 
 
+
 generations :: [(People,Relations)] -> [(People,Relations)]
-generations first = first ++ [(birth . death . change) (((take 1).(drop i)) generations) | i <- [0..]]
+generations first = first ++ [(birth.death.change) (((take 2).(drop i)) generations) | i <- [0..]]
+
+
+
+
+
+start :: Int -> Int -> [People]
+start a b = [[Person i + l | i <- [0..b]] | l <- [0..a]]
+
+
+main = do
+	n <- getLine
+	putStrLn $ show $ fst $ generations (start 1 5) !! read n
+	main
 
 
